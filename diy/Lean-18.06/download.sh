8db599b7808ddb0bd0e1331e6a9320210d7f4751
White12352 <86940955+White12352@users.noreply.github.com>
2022-07-04 20:25:57 +0800

    Update download.sh

--- diy/Lean-18.06/download.sh
+++ diy/Lean-18.06/download.sh
@@ -27,37 +27,6 @@ git clone --depth 1 https://github.com/UnblockNeteaseMusic/luci-app-unblocknetea
 git clone --depth 1 -b packages https://github.com/xiaorouji/openwrt-passwall passwall-pkgs && rm -rf passwall-pkgs/.github && mv -n passwall-pkgs/* ./ && rm -rf passwall-pkgs
 git clone --depth 1 -b luci https://github.com/xiaorouji/openwrt-passwall passwall-luci && mv -n passwall-luci/luci-app-passwall ./ && rm -rf passwall-luci
 
-git_sparse_clone master "https://github.com/coolsnowwolf/packages" "dnsforwarder_pkg" net/dnsforwarder
-git_sparse_clone master "https://github.com/coolsnowwolf/lede" "shortcut_fe_pkgs" package/lean/shortcut-fe
-git_sparse_clone master "https://github.com/coolsnowwolf/luci" "turboacc_luci" applications/luci-app-turboacc
-git_sparse_clone openwrt-21.02 "https://github.com/immortalwrt/luci" "eqos_luci" applications/luci-app-eqos
-git_sparse_clone openwrt-21.02 "https://github.com/immortalwrt/packages" "uugamebooster_pkg" net/uugamebooster
-git_sparse_clone openwrt-21.02 "https://github.com/immortalwrt/luci" "uugamebooster_luci" applications/luci-app-uugamebooster
-#git_sparse_clone openwrt-21.02 "https://github.com/immortalwrt/luci" "unblockneteasemusic_luci" applications/luci-app-unblockneteasemusic
-git_sparse_clone main "https://github.com/NueXini/NueXini_Packages" "unblockmusic_luci" luci-app-unblockmusic
-git_sparse_clone main "https://github.com/NueXini/NueXini_Packages" "unblockneteasemusic_pkg" UnblockNeteaseMusic
-#git_sparse_clone main "https://github.com/NueXini/NueXini_Packages" "unblockneteasemusic_luci" luci-app-unblockneteasemusic
-git_sparse_clone main "https://github.com/NueXini/NueXini_Packages" "unblockneteasemusic_go_pkg" UnblockNeteaseMusic-Go
-git_sparse_clone main "https://github.com/NueXini/NueXini_Packages" "unblockneteasemusic_luci" luci-app-unblockneteasemusic-go
-git_sparse_clone openwrt-21.02 "https://github.com/immortalwrt/luci" "arpbind_luci" applications/luci-app-arpbind
-git_sparse_clone openwrt-21.02 "https://github.com/immortalwrt/luci" "usb_printer_luci" applications/luci-app-usb-printer
-git_sparse_clone openwrt-21.02 "https://github.com/immortalwrt/luci" "accesscontrol_luci" applications/luci-app-accesscontrol
-git_sparse_clone openwrt-21.02 "https://github.com/immortalwrt/packages" "vlmcsd_pkg" net/vlmcsd
-git_sparse_clone openwrt-21.02 "https://github.com/immortalwrt/luci" "vlmcsd_luci" applications/luci-app-vlmcsd
-git_sparse_clone openwrt-21.02 "https://github.com/immortalwrt/luci" "cifs_mount_luci" applications/luci-app-cifs-mount
-git_sparse_clone openwrt-21.02 "https://github.com/immortalwrt/luci" "lib_fs_luci" libs/luci-lib-fs
-git_sparse_clone openwrt-21.02 "https://github.com/immortalwrt/luci" "filetransfer_luci" applications/luci-app-filetransfer
-git_sparse_clone openwrt-21.02 "https://github.com/immortalwrt/luci" "guest_wifi_luci" applications/luci-app-guest-wifi
-git_sparse_clone openwrt-21.02 "https://github.com/immortalwrt/luci" "ramfree_luci" applications/luci-app-ramfree
-#git_sparse_clone main "https://github.com/kenzok8/small-package" "adguardhome_luci" luci-app-adguardhome
-git_sparse_clone master "https://github.com/281677160/openwrt-package" "adguardhome_luci" luci-app-adguardhome
-#git_sparse_clone master "https://github.com/281677160/openwrt-package" "adguardhome_pkg" adguardhome
-git_sparse_clone master "https://github.com/sundaqiang/openwrt-packages" "wolplus_luci" luci-app-wolplus
-git_sparse_clone main "https://github.com/kenzok8/jell" "adbyby_pkg" adbyby
-git_sparse_clone main "https://github.com/kenzok8/jell" "adbyby_plus_luci" luci-app-adbyby-plus
-#git_sparse_clone master "https://github.com/281677160/openwrt-package" "unblockneteasemusic_luci" luci-app-unblockneteasemusic
-git_sparse_clone main "https://github.com/NueXini/NueXini_Packages" "redsocks2_pkg" redsocks2
-
 rm -rf ./*/.git & rm -f ./*/.gitattributes
 rm -rf ./*/.svn & rm -rf ./*/.github & rm -rf ./*/.gitignore
 
